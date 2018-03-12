0a730fda1c2283106597247be9b156f358f5d5b9
Daniel Burgner <dburgnerjr@yahoo.com>
2018-03-11 21:59:51 -0500

    D70 GameStartActivity skeleton.
    D70 of #100DaysOfCode.  Added GameStartActivity skeleton and button in
    MainActivity that links to it.  #BuiltByUdacity #AndroidDev
    #GoogleUdacityScholars

---
+++ app/src/main/java/com/danielburgnerjr/goodforce/GameStartActivity.java
@@ -0,0 +1,13 @@
+package com.danielburgnerjr.goodforce;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameStartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+    }
+}
--- app/src/main/java/com/danielburgnerjr/goodforce/MainActivity.java
+++ app/src/main/java/com/danielburgnerjr/goodforce/MainActivity.java
@@ -1,13 +1,26 @@
 package com.danielburgnerjr.goodforce;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnHowToPlay = (Button) findViewById(R.id.btnBegin);
+        btnHowToPlay.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent intA = new Intent(MainActivity.this, GameStartActivity.class);
+                startActivity(intA);
+                //Toast.makeText(getApplicationContext(), "How To Play", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
