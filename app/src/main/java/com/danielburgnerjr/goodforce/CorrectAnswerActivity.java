2542309919c9c51805381a7dbd18b013c533c751
Daniel Burgner <dburgnerjr@yahoo.com>
2018-04-03 21:08:58 -0500

    D93 Added Correct/WrongActivity shells.
    D93 of #100DaysOfCode.  Added CorrectActivity and WrongActivity shells
    for #GoodForce app.  #BuiltByUdacity #AndroidDev #GoogleUdacityScholars

---
+++ app/src/main/java/com/danielburgnerjr/goodforce/CorrectAnswerActivity.java
@@ -0,0 +1,13 @@
+package com.danielburgnerjr.goodforce;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CorrectAnswerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correct_answer);
+    }
+}
