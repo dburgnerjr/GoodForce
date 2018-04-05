e2659e66e345c020d2aef594219f6b5a0eb59696
Daniel Burgner <dburgnerjr@yahoo.com>
2018-04-04 20:46:40 -0500

    D94 FinalScoreActivity shell.
    D94 of #100DaysOfCode.  Added FinalScoreActivity shell for #GoodForce
    app.  #BuiltByUdacity #AndroidDev #GoogleUdacityScholars

---
+++ app/src/main/java/com/danielburgnerjr/goodforce/FinalScoreActivity.java
@@ -0,0 +1,13 @@
+package com.danielburgnerjr.goodforce;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinalScoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_score);
+    }
+}
