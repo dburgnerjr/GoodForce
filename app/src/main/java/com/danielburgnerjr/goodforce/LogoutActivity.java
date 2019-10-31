b7fb1c37f7dbdb66bae94b709e666ad9d7f4985b
Daniel Burgner <dburgnerjr@yahoo.com>
2019-10-30 21:55:43 -0500

    D25/D26 Migrated to AndroidX/Updated SDK
    D25/D26 of #100DaysOfCode #2019Part2 challenge.  Migrated #GoodForce
    app to #AndroidX and updated SDK to the latest version.  #AndroidDev
    #MadeWithUdacity

--- app/src/main/java/com/danielburgnerjr/goodforce/LogoutActivity.java
+++ app/src/main/java/com/danielburgnerjr/goodforce/LogoutActivity.java
@@ -1,6 +1,6 @@
 package com.danielburgnerjr.goodforce;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LogoutActivity extends AppCompatActivity {
