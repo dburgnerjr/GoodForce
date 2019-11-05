dc3c8fc9e1d68f0f450989634b774f74416e48a3
Daniel Burgner <dburgnerjr@yahoo.com>
2019-11-04 21:44:05 -0600

    D31 AboutActivity
    D31 of #100DaysOfCode #2019Part2 challenge.  Reworked AboutActivity for
    #GoodForce app.  #AndroidDev #MadeWithUdacity

--- app/src/main/java/com/danielburgnerjr/goodforce/AboutActivity.java
+++ app/src/main/java/com/danielburgnerjr/goodforce/AboutActivity.java
@@ -1,13 +1,33 @@
 package com.danielburgnerjr.goodforce;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.danielburgnerjr.goodforce.model.User;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Intent intU;
+    private User usrU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        intU = getIntent();
+        usrU = (User) intU.getSerializableExtra("User");
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intI = new Intent(AboutActivity.this, GameStartActivity.class);
+            intI.putExtra("User", usrU);
+            startActivity(intI);
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
